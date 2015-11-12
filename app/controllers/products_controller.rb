6d0f4cefe57f10f2225eaa12c3eb31a3bda84e74
PatrickZanowski <zanowski26@hotmail.com>
2015-11-12 16:17:43 -0500

    Creating a user model, controller and view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,19 @@
 class ProductsController < ApplicationController
+
   def index
+  	@products = Product.all
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def new
+  	@user = User.new
   end
 
   def edit
+  	@user = Product.find(params[:id])
   end
+  
 end
