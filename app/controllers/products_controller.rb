c824caf908149d3c1d00a3002ce8d0bc744c5394
PatrickZanowski <zanowski26@hotmail.com>
2015-11-13 12:46:36 -0500

    Fixed review issues

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,15 +5,50 @@ class ProductsController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
+  	@product = Product.find(params[:id])
+
+  	if current_user
+  		@review = @product.reviews.build
+  	end
   end
 
   def new
-  	@user = User.new
+  	@product = Product.new
   end
 
   def edit
-  	@user = Product.find(params[:id])
+  	@product = Product.find(params[:id])
   end
-  
+
+  def create
+  	@product = Product.new(product_params)
+
+  	if @product.save
+  	  redirect_to products_path
+    else
+  	  render "new"
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update_attributes(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,24 @@
 class ReviewsController < ApplicationController
 	before_filter :ensure_logged_in, only: [:create, :destroy]
+  before_filter :load_product
+  def new
+  @review = Review.new
+  end 
+
   def show
   	@review = Review.find(params[:id])
   end
 
   def create
-  	@review = @product.reviews.build(review_params)
-    @review.user = current_user
+  	# @review = @product.reviews.build(review_params)
+   #  @review.user = current_user
+
+      @review = Review.new(
+      comment: params[:review][:comment],
+      product_id: @product.id,
+      user_id: current_user.id
+     )    
+
 
     if @review.save
     	redirect_to products_path, notice: 'Review created successfully'
