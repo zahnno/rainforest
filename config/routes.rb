89b576d14f958431dfc52f71b9995f83779316af
PatrickZanowski <zanowski26@hotmail.com>
2016-01-15 15:55:07 -0500

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+root 'products#index'
+
  resources :sessions, only: [:new, :create, :destroy]
  resources :products do
   resources :reviews, only: [:show, :create, :destroy]
